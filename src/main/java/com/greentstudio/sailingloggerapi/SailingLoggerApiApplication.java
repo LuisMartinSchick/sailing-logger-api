c5e6d5e2f7aa76670bb8a88f90bcea0b0751a6f4
Luis Martin Schick <luis3720@hotmail.com>
2021-03-02 14:38:26 +0100

    Initial commit

---
+++ src/main/java/com/greentstudio/sailingloggerapi/SailingLoggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.greentstudio.sailingloggerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailingLoggerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailingLoggerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greentstudio/sailingloggerapi/SailingLoggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greentstudio.sailingloggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailingLoggerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
